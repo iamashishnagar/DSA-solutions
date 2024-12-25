88e464f298fe50c22599cdf1599d609a39c657e9
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2024-12-25 12:50:26 -0500

    Time: 1 ms (82.73%) | Memory: 43 MB (68.08%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        backtrack(0, 0, n, result, "");
+        backtrack(0, 0, n, result, new StringBuilder());
         return result;
     }
 
-    private void backtrack(int openN, int closeN, int n, List<String> result, String current){
+    private void backtrack(int openN, int closeN, int n, List<String> result, StringBuilder current){
         if(openN == closeN && openN == n){
-            result.add(current);
+            result.add(current.toString());
             return;
         }
 
-        if(openN < n)
-            backtrack(openN + 1, closeN, n, result, current + "(");
-        
-        if(closeN < openN)
-            backtrack(openN, closeN + 1, n, result, current + ")");
+        if(openN < n){
+            backtrack(openN + 1, closeN, n, result, current.append("("));
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        if(closeN < openN){
+            backtrack(openN, closeN + 1, n, result, current.append(")"));
+            current.deleteCharAt(current.length() - 1);
+        }
     }
 }
