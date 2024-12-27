a99780b690b31898453fc1f24869b56674c37494
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2024-12-27 12:58:12 -0500

    Time: 3 ms (87.90%) | Memory: 44.6 MB (88.15%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() == 0 || t.length() == 0 || s.length() < t.length() )
-            return "";
+        int m = s.length();
+        int n = t.length();
+        if(m < n) return "";
 
-        int[] tMap = new int[128];
-        for(int i = 0; i < t.length(); i++){
-            tMap[t.charAt(i)]++;
-        }
+        int[] count = new int[128]; //ASCII
+        for(char c : t.toCharArray())
+            count[c]--;
+        
+        int min = Integer.MAX_VALUE, start = 0, left = 0;
 
-        int left = 0, right = 0;
-        int requiredCount = t.length();
-        int startIndex = 0;
-        int minLength = Integer.MAX_VALUE;
+        for(int right = 0; right < m; right++){
+            if(count[s.charAt(right)] < 0) n--;
+            count[s.charAt(right)]++;
 
-        while(right < s.length()){
-            if(tMap[s.charAt(right)] > 0)
-                requiredCount--;
-            tMap[s.charAt(right)]--;
-            right++;
-
-            while(requiredCount == 0){
-                int length = right - left;
-                if(length < minLength){
-                    minLength = length;
-                    startIndex = left;
+            while(n == 0){
+                int window = right - left + 1;
+                if(window < min){
+                    min = window;
+                    start = left;
                 }
 
-                tMap[s.charAt(left)]++;
-                if(tMap[s.charAt(left)] > 0){
-                    requiredCount++;
-                }
+                count[s.charAt(left)]--;
+                if(count[s.charAt(left)] < 0) n++;
                 left++;
             }
         }
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLength);
+
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
+        
     }
 }
