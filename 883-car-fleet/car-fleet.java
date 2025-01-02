8040a6a63d3dafeab210ecc0cc32dbe858818a4f
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-01 19:13:47 -0500

    Time: 80 ms (59.06%) | Memory: 58.3 MB (32.62%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -2,6 +2,7 @@ class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
         int[][] pair = new int[n][2];
+
         for(int i = 0; i < n; i++){
             pair[i][0] = position[i];
             pair[i][1] = speed[i];
@@ -21,5 +22,6 @@ class Solution {
         }
 
         return fleet;
+        
     }
 }
