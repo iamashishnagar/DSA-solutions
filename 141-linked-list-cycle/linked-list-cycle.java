b0696365607583dd926a09f163f93dbed49f6fab
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-09 08:18:37 -0500

    Time: 0 ms (100.00%) | Memory: 44.5 MB (57.12%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -18,8 +18,7 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow == fast)
-                return true;
+            if(slow == fast) return true;
         }
 
         return false;
