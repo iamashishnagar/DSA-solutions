2c9d84359f9a4306e564efc3dffcd02493c3cf7d
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-09 10:00:32 -0500

    Time: 0 ms (100.00%) | Memory: 44.4 MB (60.85%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,6 +16,7 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if(head == null) return null;
+        //append copied list with val and next along side original list
         Node current = head;
         while(current != null){
             Node newNode = new Node(current.val);
@@ -23,24 +24,25 @@ class Solution {
             current.next = newNode;
             current = current.next.next;
         }
-
+        //iterate through the new combined list to fill random number
         current = head;
         while(current != null){
             if(current.random != null)
                 current.next.random = current.random.next;
             current = current.next.next;
         }
-
+        //split the list into original and copied
         current = head;
         Node copied = head.next;
         while(current != null){
-            Node copiedNode = current.next;
-            current.next = copiedNode.next;
+            Node copiedNext = current.next;
+            current.next = copiedNext.next;
             current = current.next;
-            if(copiedNode.next != null)
-                copiedNode.next = copiedNode.next.next;
+            if(copiedNext.next != null){
+                copiedNext.next = copiedNext.next.next;
+            }
         }
-
+        //return copied head  
         return copied;
     }
 }
