cfd5c68230f6c1f390cf18e9f85acf58729c4a28
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-13 16:42:44 -0500

    Time: 5 ms (97.43%) | Memory: 44.6 MB (61.79%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,25 +3,24 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         for(String token : tokens){
             switch(token){
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-                case "-":
-                    int b = stack.pop();
-                    int a = stack.pop();
-                    stack.push(a - b);
-                    break;
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
-                case "/":
-                    b = stack.pop();
-                    a = stack.pop();
-                    stack.push(a / b);
-                    break;
+                case "+": 
+                        stack.push(stack.pop() + stack.pop());
+                        break;
+                case "-": 
+                        int a = stack.pop(), b = stack.pop();
+                        stack.push(b - a);
+                        break;
+                case "*": 
+                        stack.push(stack.pop() * stack.pop());
+                        break;
+                case "/": 
+                        a = stack.pop(); b = stack.pop();
+                        stack.push(b / a);
+                        break;
                 default:
-                    stack.push(Integer.parseInt(token));
-                    break;
+                        stack.push(Integer.parseInt(token));
+                        break;
+                
             }
         }
         return stack.pop();
