9038d12c7dfa22f4986ccfea801ee8e3c99f9754
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-12 22:45:07 -0500

    Time: 8 ms (58.61%) | Memory: 42.7 MB (63.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+        int m = s1.length(), n = s2.length();
         if(m > n) return false;
-
-        int[] s1Arr = new int[26];
-        int[] s2Arr = new int[26];
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
 
         for(int i = 0; i < m; i++){
-            s1Arr[s1.charAt(i) - 'a']++;
-            s2Arr[s2.charAt(i) - 'a']++;
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
         }
 
-        int left = 0, right = m - 1;
-
-        for(int i = 0; i < n - m; i++){
-            if (match(s1Arr, s2Arr)) return true;
-
-            s2Arr[s2.charAt(i) - 'a']--;
-            s2Arr[s2.charAt(i + m) - 'a']++;
-
+        for(int right = m; right < n; right++){
+            int left = right - m;
+            if(isMatch(s1Count, s2Count)) return true;
+            s2Count[s2.charAt(left) - 'a']--;
+            s2Count[s2.charAt(right) - 'a']++;
         }
-        return match(s1Arr, s2Arr);
+        return isMatch(s1Count, s2Count);
     }
 
-    private boolean match(int[] s1Arr, int[] s2Arr){
-        for(int i = 0; i < 26; i++){
-            if(s1Arr[i] != s2Arr[i])
+    private boolean isMatch(int[] s1Count, int[] s2Count){
+        for(int i = 0; i < 26; i++)
+            if(s1Count[i] != s2Count[i])
                 return false;
-        }
-
         return true;
     }
 }
