29818703157dc6145032ba340d96d810b1a0db81
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-12 23:26:18 -0500

    Time: 3 ms (87.92%) | Memory: 45.1 MB (51.83%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,34 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
+        int m = s.length(), n = t.length();
         if(m < n) return "";
-        int[] tMap = new int[128];
-        for(char c : t.toCharArray())
-            tMap[c]++;
-
-        int need = n;
-        int have = 0;
-        int start = 0;
-        int min = Integer.MAX_VALUE;
-        int left = 0;
+        int[] count = new int[128];
+        for(int i = 0; i < n; i++)
+            count[t.charAt(i)]++;
+        
+        int need = n, have = 0;
+        int left = 0, start = 0, minLength = Integer.MAX_VALUE;
 
         for(int right = 0; right < m; right++){
-            if(tMap[s.charAt(right)] > 0) have++;
-            tMap[s.charAt(right)]--;
+            if(count[s.charAt(right)] > 0) have++;
+            count[s.charAt(right)]--;
 
             while(have == need){
-                int length = right - left + 1;
-                if(length < min){
-                    min = length;
+                int window = right - left + 1;
+                if(window < minLength){
+                    minLength = window;
                     start = left;
                 }
-                tMap[s.charAt(left)]++;
-                if(tMap[s.charAt(left)] > 0) have--; // Only decrement if it was part of t
+                if(count[s.charAt(left)] == 0) have--;
+                count[s.charAt(left)]++;
                 left++;
             }
         }
-
-        return (min == Integer.MAX_VALUE) ? "" : s.substring(start, start + min);      
+         return (minLength == Integer.MAX_VALUE) ? "" : s.substring(start, start + minLength);
+         
     }
 }
