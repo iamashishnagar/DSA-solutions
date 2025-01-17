7515ab98ff0a5fb5c744621753f3af948cf5ad28
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-17 10:32:49 -0500

    Time: 1 ms (100.00%) | Memory: 46.3 MB (40.07%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,26 +3,26 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
         if(m > n) return findMedianSortedArrays(nums2, nums1);
-
         int total = m + n;
         int half = (total + 1) / 2;
+
         int left = 0, right = m;
         while(left <= right){
             int mid1 = left + (right - left) / 2;
             int mid2 = half - mid1;
 
-            int left1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int left2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int right1 = mid1 < m ? nums1[mid1] : Integer.MAX_VALUE;
-            int right2 = mid2 < n ? nums2[mid2] : Integer.MAX_VALUE;
+            int left1Max = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int left2Max = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int right1Min = (mid1 == m) ? Integer.MAX_VALUE : nums1[mid1];
+            int right2Min = (mid2 == n) ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(left1 <= right2 && left2 <= right1){
+            if(left1Max <= right2Min && left2Max <= right1Min){
                 if(total % 2 == 0)
-                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                    return (double) (Math.max(left1Max, left2Max) + Math.min(right1Min, right2Min)) / 2.0;
                 else
-                    return Math.max(left1, left2);
+                    return (double) Math.max(left1Max, left2Max);
             }
-            else if(left1 > right2)
+            else if(left1Max > right2Min)
                 right = mid1 - 1;
             else left = mid1 + 1;
         }
