99f1c0b04ff3e39bda9a31b8f81b78b43cb9cad6
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-17 17:24:32 -0500

    Time: 1 ms (71.43%) | Memory: 42.1 MB (74.70%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -22,20 +22,17 @@ class Solution {
 
         while(!queue.isEmpty()){
             int size = queue.size();
+            TreeNode rightMost = null;
             while(size > 0){
-                TreeNode current = queue.poll();;
-                if(size == 1)
-                    result.add(current.val);
-                
-                if(current.left != null)
-                    queue.add(current.left);
-                if(current.right != null)
-                    queue.add(current.right);  
+                TreeNode current = queue.poll();
+                rightMost = current;
+                if(current.left != null) queue.add(current.left);
+                if(current.right != null) queue.add(current.right);
 
                 size--;
             }
+            result.add(rightMost.val);
         }
-
         return result;
     }
 }
