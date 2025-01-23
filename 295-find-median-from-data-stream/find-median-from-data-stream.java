8a513be7d7b9d266c7685156ed6432ff34a9b2c1
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-23 13:20:58 -0500

    Time: 107 ms (44.64%) | Memory: 63.2 MB (94.38%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,35 @@
+class MedianFinder {
+    Queue<Integer> maxHeap; //left sorted
+    Queue<Integer> minHeap; //right sorted
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+
+        if(!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek())
+            minHeap.offer(maxHeap.poll());
+        
+        if(maxHeap.size() > minHeap.size() + 1)
+            minHeap.offer(maxHeap.poll());
+        else if(minHeap.size() > maxHeap.size())
+            maxHeap.offer(minHeap.poll());
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size() > minHeap.size())
+            return maxHeap.peek();
+        else
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
