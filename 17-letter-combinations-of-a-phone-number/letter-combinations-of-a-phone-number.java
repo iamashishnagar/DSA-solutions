8ec53a3b8bd83e6a113d5720146f12bdcb6e5f0c
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-28 14:57:29 -0500

    Time: 0 ms (100.00%) | Memory: 42 MB (88.66%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,35 +2,36 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         if(digits == null || digits.length() == 0)
             return new ArrayList<>();
-        String[] digitToLetters = {
-            "", //0
-            "", //1
-            "abc", //2
-            "def", //3
-            "ghi", //4
-            "jkl", //5
-            "mno", //6
-            "pqrs", //7
-            "tuv", //8
-            "wxyz" //9
+        
+        String[] digitsToLetters = {
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
         };
 
         List<String> result = new ArrayList<>();
-        backtrack(0, digits, digitToLetters, new StringBuilder(), result);
+        backtrack(0, digits, digitsToLetters, new StringBuilder(), result);
         return result;
     }
 
-    private void backtrack(int index, String digits, String[] digitToLetters, StringBuilder current, List<String> result){
+    private void backtrack(int index, String digits, String[] digitsToLetters, StringBuilder current, List<String> result){
         if(index == digits.length()){
             result.add(current.toString());
             return;
         }
 
-        String letters = digitToLetters[digits.charAt(index) - '0'];
+        String letters = digitsToLetters[digits.charAt(index) - '0'];
 
         for(char letter : letters.toCharArray()){
             current.append(letter);
-            backtrack(index + 1, digits, digitToLetters, current, result);
+            backtrack(index + 1, digits, digitsToLetters, current, result);
             current.deleteCharAt(current.length() - 1);
         }
     }
