03edbc1c8ae2e18d24b94793deccb73d2c469fc1
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-29 10:45:53 -0500

    Time: 133 ms (81.92%) | Memory: 41.7 MB (48.98%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,29 +2,26 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0))
+                if(board[i][j] == word.charAt(0) && backtrack(board, word, 0, i, j))
                     return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int index){
+    private boolean backtrack(char[][] board, String word, int index, int row, int col){
         if(index == word.length()) return true;
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length ||
-            board[i][j] != word.charAt(index))
-            return false;
-
-        char visited = board[i][j];
-        board[i][j] = '#';
-
-        boolean found = dfs(board, word, i - 1, j, index + 1) ||
-                        dfs(board, word, i + 1, j, index + 1) ||
-                        dfs(board, word, i, j - 1, index + 1) ||
-                        dfs(board, word, i, j + 1, index + 1);
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) return false;
         
-        board[i][j] = visited;
+        char letter = board[row][col];
+        board[row][col] = '#';
 
+        boolean found = backtrack(board, word, index + 1, row + 1, col) ||
+                        backtrack(board, word, index + 1, row - 1, col) ||
+                        backtrack(board, word, index + 1, row, col + 1) ||
+                        backtrack(board, word, index + 1, row, col - 1);
+
+        board[row][col] = letter;
         return found;
     }
 }
