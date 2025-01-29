331c6d4dee3c95d6e6197fdbd6f58b0d7fd4884c
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-01-29 11:00:59 -0500

    Time: 1 ms (99.74%) | Memory: 43.5 MB (61.66%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -8,10 +8,9 @@ class Solution {
 
     private void backtrack(int index, int[] nums, List<Integer> subset, List<List<Integer>> result){
         result.add(new ArrayList<>(subset));
-
+        
         for(int i = index; i < nums.length; i++){
             if(i > index && nums[i] == nums[i - 1]) continue;
-
             subset.add(nums[i]);
             backtrack(i + 1, nums, subset, result);
             subset.remove(subset.size() - 1);
