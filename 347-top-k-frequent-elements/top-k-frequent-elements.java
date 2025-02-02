ae188e5a17c6349328d1faf65810aead64fb4141
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-02 08:42:56 -0500

    Time: 15 ms (38.15%) | Memory: 48.9 MB (42.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        Queue<Integer> minHeap = new PriorityQueue<>(
-            (a, b) -> map.get(a) - map.get(b)
-        );
-
-        for(int key : map.keySet()){
-            minHeap.add(key);
-            if(minHeap.size() > k)
-                minHeap.poll();
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num : nums)
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        
+        Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> freqMap.get(a) - freqMap.get(b));
+        for(int key : freqMap.keySet()){
+            maxHeap.offer(key);
+            if(maxHeap.size() > k)
+                maxHeap.poll();
         }
 
         int[] result = new int[k];
-        for(int i = 0; i < k; i++)
-            result[i] = minHeap.poll();
+        for(int i = 0; i < k && !maxHeap.isEmpty(); i++)
+            result[i] = maxHeap.poll();
 
         return result;
     }
