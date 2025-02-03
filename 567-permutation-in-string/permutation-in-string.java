8026b37921d4fd6dada5bd50d420e9088f588e56
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-03 13:09:05 -0500

    Time: 8 ms (60.68%) | Memory: 42.8 MB (58.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        if(m > n) return false;
+        if(s1.length() > s2.length()) return false;
         int[] s1Count = new int[26];
         int[] s2Count = new int[26];
-
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < s1.length(); i++){
             s1Count[s1.charAt(i) - 'a']++;
             s2Count[s2.charAt(i) - 'a']++;
         }
-
-        for(int right = m; right < n; right++){
-            int left = right - m;
+        int left = 0, right = s1.length() - 1;
+        while(right < s2.length() - 1){
             if(isMatch(s1Count, s2Count)) return true;
-            s2Count[s2.charAt(left) - 'a']--;
-            s2Count[s2.charAt(right) - 'a']++;
+            s2Count[s2.charAt(left++) - 'a']--;
+            s2Count[s2.charAt(++right) - 'a']++;
         }
         return isMatch(s1Count, s2Count);
     }
 
     private boolean isMatch(int[] s1Count, int[] s2Count){
-        for(int i = 0; i < 26; i++)
+        for(int i = 0; i < 26; i++){
             if(s1Count[i] != s2Count[i])
                 return false;
+        }
         return true;
     }
 }
