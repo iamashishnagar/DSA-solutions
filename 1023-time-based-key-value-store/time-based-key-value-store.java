201a360a4fe82a0f6518246dcb5a24d243c155ee
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-05 09:46:35 -0500

    Time: 139 ms (66.45%) | Memory: 118.4 MB (82.82%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,43 +1,39 @@
 class TimeMap {
-    class Pair{
-        int timestamp;
+    class TimeStore{
         String value;
+        int timestamp;
 
-        Pair(int timestamp, String value){
-            this.timestamp = timestamp;
+        TimeStore(String value, int timestamp){
             this.value = value;
+            this.timestamp = timestamp;
         }
     }
 
-    Map<String, List<Pair>> map;
+    Map<String, List<TimeStore>> map;
 
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key, k -> new ArrayList());
-        map.get(key).add(new Pair(timestamp, value));
+        map.computeIfAbsent(key, k -> new ArrayList<>());
+        map.get(key).add(new TimeStore(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
         if(!map.containsKey(key)) return "";
-        List<Pair> values = map.get(key);
+        List<TimeStore> values = map.get(key);
         int left = 0, right = values.size() - 1;
         String result = "";
-
         while(left <= right){
             int mid = left + (right - left) / 2;
-            int timestampPrev = values.get(mid).timestamp;
-            if(timestampPrev <= timestamp){
-                result =  values.get(mid).value;
+            if(values.get(mid).timestamp <= timestamp){
+                result = values.get(mid).value;
                 left = mid + 1;
             }
             else right = mid - 1;
         }
-
         return result;
-
     }
 }
 
