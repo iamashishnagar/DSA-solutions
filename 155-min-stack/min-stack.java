d46673102ccdf6e270d71b59bc5c89611ca46795
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-05 09:04:51 -0500

    Time: 4 ms (97.12%) | Memory: 44.8 MB (68.11%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,35 +1,36 @@
 import java.util.*;
+
 class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
 
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
     }
     
     public void push(int val) {
         stack.push(val);
-        if(minStack.isEmpty() || val <= minStack.peek())
+        if(minStack.isEmpty() || minStack.peek() >= val)
             minStack.push(val);
     }
     
     public void pop() {
         if(!stack.isEmpty()){
             int removed = stack.pop();
-            if(!minStack.isEmpty() && removed == minStack.peek())
+            if(!minStack.isEmpty() && minStack.peek() == removed)
                 minStack.pop();
         }
     }
     
     public int top() {
         if(!stack.isEmpty()) return stack.peek();
-        throw new NoSuchElementException("Stack is empty!");
+        throw new NoSuchElementException("Stack is empty.");
     }
     
     public int getMin() {
         if(!minStack.isEmpty()) return minStack.peek();
-        throw new NoSuchElementException("Stack is empty!");
+        throw new NoSuchElementException("Stack is empty.");
     }
 }
 
