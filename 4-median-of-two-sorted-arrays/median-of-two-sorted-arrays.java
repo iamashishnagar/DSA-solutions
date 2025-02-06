7c23b34a004c1061fa120a2e75238f88cb607734
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-06 13:03:11 -0500

    Time: 1 ms (100.00%) | Memory: 45.8 MB (96.51%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,8 +1,8 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length > nums2.length)
+        if(nums2.length < nums1.length)
             return findMedianSortedArrays(nums2, nums1);
-
+        
         int m = nums1.length, n = nums2.length;
         int leftHalf = (m + n + 1) / 2;
         int left = 0, right = m;
@@ -11,17 +11,17 @@ class Solution {
             int mid1 = left + (right - left) / 2;
             int mid2 = leftHalf - mid1;
 
-            int maxLeft1 = mid1 > 0 ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int minRight1 = mid1 < m ? nums1[mid1] : Integer.MAX_VALUE;
-            int maxLeft2 = mid2 > 0 ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int minRight2 = mid2 < n ? nums2[mid2] : Integer.MAX_VALUE;
+            int left1Max = mid1 > 0 ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int left2Max = mid2 > 0 ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int right1Min = mid1 < m ? nums1[mid1] : Integer.MAX_VALUE;
+            int right2Min = mid2 < n ? nums2[mid2] : Integer.MAX_VALUE;
 
-            if(maxLeft1 <= minRight2 && maxLeft2 <= minRight1){
+            if(left1Max <= right2Min && left2Max <= right1Min)
                 return (m + n) % 2 != 0
-                    ? Math.max(maxLeft1, maxLeft2)
-                    : (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
-            }
-            else if(maxLeft1 > minRight2) right = mid1 - 1;
+                    ? Math.max(left1Max, left2Max)
+                    : (Math.max(left1Max, left2Max) + Math.min(right1Min, right2Min)) / 2.0;
+            else if(left1Max > right2Min)
+                right = mid1 - 1;
             else left = mid1 + 1;
         }
 
