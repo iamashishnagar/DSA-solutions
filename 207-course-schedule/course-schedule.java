74571320be5ea3a9c0114313e402bf522ffd63f7
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-07 11:48:20 -0500

    Time: 7 ms (50.44%) | Memory: 45.8 MB (36.40%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    Map<Integer, List<Integer>> preMap = new HashMap<>();
+    Set<Integer> visiting = new HashSet<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for(int i = 0; i < numCourses; i++)
+            preMap.put(i, new ArrayList<>());
+
+        for(int[] pre : prerequisites)
+            preMap.get(pre[0]).add(pre[1]);
+        
+        for(int i = 0; i < numCourses; i++)
+            if(!canFinishCourse(i))
+                return false;
+        
+        return true;
+    }
+
+    private boolean canFinishCourse(int course){
+        if(visiting.contains(course)) return false;
+        if(preMap.get(course).isEmpty()) return true;
+
+        visiting.add(course);
+
+        for(int pre : preMap.get(course))
+            if(!canFinishCourse(pre))
+                return false;
+
+        visiting.remove(course);
+        preMap.put(course, new ArrayList<>());
+
+        return true;
+    }
+}
