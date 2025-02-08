0ee3794f62e9f2ecfe6aa0c53e7ce94abaaac52f
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-08 09:01:35 -0500

    Time: 2 ms (70.56%) | Memory: 44.4 MB (80.13%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,26 +1,34 @@
 class Solution {
-    int maxArea = 0;
     int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        if(grid.length == 0) return maxArea;
+
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1)
-                    maxArea = Math.max(maxArea, calculateArea(grid, i, j));
+                if(grid[i][j] == 1){
+                    maxArea = Math.max(maxArea, dfs(i, j, grid));
+                }
             }
         }
+
         return maxArea;
     }
 
-    private int calculateArea(int[][] grid, int row, int col){
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == 0)
+    private int dfs(int i, int j, int[][] grid){
+        if(i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == 0)
             return 0;
         
-        grid[row][col] = 0;
+        grid[i][j] = 0;
         int area = 1;
 
-        for(int[] dir : directions)
-            area += calculateArea(grid, row + dir[0], col + dir[1]);
+        for(int[] dir : directions){
+            int row = i + dir[0];
+            int col = j + dir[1];
+
+            area += dfs(row, col, grid);
+        }
         
         return area;
     }
