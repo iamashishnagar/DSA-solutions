3201275bd053b687031998b94761548fec56ec4a
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-08 10:04:23 -0500

    Time: 2 ms (71.01%) | Memory: 43 MB (12.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<int[]> queue = new ArrayDeque<>();
-        int freshCount = 0;
+    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+    public int orangesRotting(int[][] grid) {
+        if(grid.length == 0) return 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        int fresh = 0;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 2)
-                    queue.offer(new int[]{i, j});
+                    queue.add(new int[] {i, j});
                 else if(grid[i][j] == 1)
-                    freshCount++;
+                    fresh++;
             }
         }
 
-        if(freshCount == 0) return 0;
+        if(fresh == 0) return 0;
         int minutes = 0;
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while(!queue.isEmpty()){
             int size = queue.size();
@@ -28,11 +28,11 @@ class Solution {
                     int row = cell[0] + dir[0];
                     int col = cell[1] + dir[1];
 
-                    if(row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == 1){
+                    if(row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == 1){
                         grid[row][col] = 2;
-                        queue.offer(new int[]{row, col});
-                        freshCount--;
+                        queue.add(new int[] {row, col});
                         rotted = true;
+                        fresh--;
                     }
                 }
                 size--;
@@ -40,6 +40,7 @@ class Solution {
             if(rotted) minutes++;
         }
 
-        return (freshCount == 0) ? minutes : -1;
+        return fresh == 0 ? minutes : -1;
+
     }
 }
