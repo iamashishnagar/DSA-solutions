eeb6869f1c6dac3258ed0e3fe3338928e937c74a
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-08 09:18:35 -0500

    Time: 4 ms (97.73%) | Memory: 45.9 MB (40.00%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -2,20 +2,22 @@ class Solution {
     int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(heights.length == 0) return result;
+
         int rows = heights.length, cols = heights[0].length;
         boolean[][] pacificVisited = new boolean[rows][cols];
         boolean[][] atlanticVisited = new boolean[rows][cols];
 
-        for(int row = 0; row < rows; row++){
-            dfs(row, 0, pacificVisited, heights);
-            dfs(row, cols - 1, atlanticVisited, heights);
-        }
         for(int col = 0; col < cols; col++){
             dfs(0, col, pacificVisited, heights);
             dfs(rows - 1, col, atlanticVisited, heights);
         }
+        for(int row = 0; row < rows; row++){
+            dfs(row, 0, pacificVisited, heights);
+            dfs(row, cols - 1, atlanticVisited, heights);
+        }
 
-        List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(pacificVisited[i][j] && atlanticVisited[i][j])
@@ -27,13 +29,16 @@ class Solution {
     }
 
     private void dfs(int i, int j, boolean[][] visited, int[][] heights){
+        if(i < 0 || j < 0 || i == heights.length || j == heights[0].length || visited[i][j])
+            return;
+        
         visited[i][j] = true;
 
         for(int[] dir : directions){
             int row = i + dir[0];
             int col = j + dir[1];
 
-            if(row >= 0 && col >= 0 && row < heights.length && col < heights[0].length && !visited[row][col] && heights[row][col] >= heights[i][j])
+            if(row >= 0  && col >= 0 && row < heights.length && col < heights[0].length && !visited[row][col] && heights[row][col] >= heights[i][j])
                 dfs(row, col, visited, heights);
         }
     }
