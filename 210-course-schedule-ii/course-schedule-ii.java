ca511ea5e1ab36f0a9c5aec573bfdc741b4df5d5
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-11 10:51:48 -0500

    Time: 5 ms (77.85%) | Memory: 46.3 MB (27.73%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,44 +1,38 @@
 class Solution {
-    Map<Integer, List<Integer>> preMap;
-    Set<Integer> visiting;
-    Set<Integer> completed;
-    List<Integer> order;
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        preMap = new HashMap<>();
-        visiting = new HashSet<>();
-        completed = new HashSet<>();
-        order = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inorder = new int[numCourses];
+        List<Integer> order = new ArrayList<>();
 
         for(int i = 0; i < numCourses; i++)
-            preMap.put(i, new ArrayList<>());
-        
-        for(int[] pre : prerequisites)
-            preMap.get(pre[0]).add(pre[1]);
+            graph.add(new ArrayList<>());
 
-        for(int course = 0; course < numCourses; course++)
-            if(!canFinishCourse(course)) return new int[0];
+        for(int[] pre : prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+            inorder[pre[0]]++;
+        }
 
-        int[] result = new int[order.size()];
-        for(int i = 0; i < result.length; i++)
-            result[i] = order.get(i);
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < inorder.length; i++)
+            if(inorder[i] == 0) queue.offer(i);
 
-        return result;
-    }
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            order.add(current);
 
-    private boolean canFinishCourse(int course){
-        if(visiting.contains(course)) return false;
-        if(completed.contains(course)) return true;
+            for(int next : graph.get(current)){
+                inorder[next]--;
+                if(inorder[next] == 0) queue.offer(next);
+            }
+        }
 
-        visiting.add(course);
+        if(order.size() == numCourses){
+            int[] result = new int[numCourses];
+            for(int i = 0; i < numCourses; i++)
+                result[i] = order.get(i);
 
-        for(int pre : preMap.get(course))
-            if(!canFinishCourse(pre)) return false;
-        
-        visiting.remove(course);
-        completed.add(course);
-        order.add(course);
-
-        return true;
+            return result;
+        }
+        else return new int[0];
     }
 }
