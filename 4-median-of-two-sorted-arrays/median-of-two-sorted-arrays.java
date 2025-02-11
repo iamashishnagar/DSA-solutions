f05f654ab1689bababa525975980050ec23c06ef
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-10 21:57:44 -0500

    Time: 1 ms (100.00%) | Memory: 45.5 MB (99.67%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,9 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums2.length < nums1.length)
-            return findMedianSortedArrays(nums2, nums1);
-        
         int m = nums1.length, n = nums2.length;
+        if(m > n) return findMedianSortedArrays(nums2, nums1);
         int leftHalf = (m + n + 1) / 2;
         int left = 0, right = m;
 
@@ -11,20 +9,20 @@ class Solution {
             int mid1 = left + (right - left) / 2;
             int mid2 = leftHalf - mid1;
 
-            int left1Max = mid1 > 0 ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int left2Max = mid2 > 0 ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int right1Min = mid1 < m ? nums1[mid1] : Integer.MAX_VALUE;
-            int right2Min = mid2 < n ? nums2[mid2] : Integer.MAX_VALUE;
+            int leftMax1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int leftMax2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int rightMin1 = mid1 == m ? Integer.MAX_VALUE : nums1[mid1];
+            int rightMin2 = mid2 == n ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(left1Max <= right2Min && left2Max <= right1Min)
+            if(leftMax1 <= rightMin2 && leftMax2 <= rightMin1)
                 return (m + n) % 2 != 0
-                    ? Math.max(left1Max, left2Max)
-                    : (Math.max(left1Max, left2Max) + Math.min(right1Min, right2Min)) / 2.0;
-            else if(left1Max > right2Min)
+                    ? Math.max(leftMax1, leftMax2)
+                    : (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2.0;
+            else if(leftMax1 > rightMin2)
                 right = mid1 - 1;
             else left = mid1 + 1;
         }
 
-        throw new IllegalArgumentException("Input arrays are not sorted");
+        throw new IllegalArgumentException("Input arrays are not sorted.");
     }
 }
