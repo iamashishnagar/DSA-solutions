dfcca2ce3d5ff1928b0ccf41d1ba3059137390bb
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-02-12 20:24:11 -0500

    Time: 65 ms (84.01%) | Memory: 45.6 MB (88.78%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,7 +2,6 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>(wordList);
         if(!set.contains(endWord)) return 0;
-
         Queue<Pair<String, Integer>> queue = new ArrayDeque<>();
         queue.offer(new Pair<>(beginWord, 1));
 
@@ -13,22 +12,23 @@ class Solution {
 
             if(word.equals(endWord)) return steps;
 
-            char[] wordArray = word.toCharArray();
-            for(int i = 0; i < wordArray.length; i++){
-                char original = wordArray[i];
+            char[] letters = word.toCharArray();
+            for(int i = 0; i < letters.length; i++){
+                char original = letters[i];
 
                 for(char c = 'a'; c <= 'z'; c++){
-                    if(c == original) continue;
+                    if(c != original){
+                        letters[i] = c;
+                        String transformed = new String(letters);
 
-                    wordArray[i] = c;
-                    String newWord = new String(wordArray);
-
-                    if(set.contains(newWord)){
-                        queue.offer(new Pair<>(newWord, steps + 1));
-                        set.remove(newWord);
+                        if(set.contains(transformed)){
+                            queue.offer(new Pair<>(transformed, steps + 1));
+                            set.remove(transformed);
+                        }
                     }
                 }
-                wordArray[i] = original;
+
+                letters[i] = original;
             }
         }
 
