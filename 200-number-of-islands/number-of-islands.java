96aa84dba39a94a7da8c1f6d9605812330c098a8
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-04-04 14:34:11 -0400

    Time: 3 ms (86.04%) | Memory: 49.3 MB (85.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,34 @@
 class Solution {
-    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int m, n;
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int numIslands(char[][] grid) {
-        int total = 0;
-        if(grid.length == 0) return total;
+        m = grid.length;
+        n = grid[0].length;
+        if(m == 0) return 0;
+        int result = 0;
 
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    total++;
-                    dfs(i, j, grid);
+                    result++;
+                    bfs(i, j, grid);
                 }
             }
         }
 
-        return total;
+        return result;
     }
 
-    private void dfs(int i, int j, char[][] grid){
-        grid[i][j] = '0';
-
+    private void bfs(int i, int j, char[][] grid){
+        if(i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0')
+            return;
+        
+        if(grid[i][j] == '1')
+            grid[i][j] = '0';
+        
         for(int[] dir : directions){
-            int row = i + dir[0];
-            int col = j + dir[1];
-
-            if(row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == '1')
-                dfs(row, col, grid);
+            bfs(i + dir[0], j + dir[1], grid);
         }
     }
 }
