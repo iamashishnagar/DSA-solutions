4d3f2ab8efff15ac6b6c2f6f1dcefaa273c653be
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-04-08 12:24:39 -0400

    Time: 0 ms (100.00%) | Memory: 40.6 MB (37.83%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,17 +1,12 @@
 class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+
     public int tribonacci(int n) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        int first = 0, second = 1, third = 1;
-        int result = 0;
-        
-        for(int i = 3; i <= n; i++){
-            result = first + second + third;
-            first = second;
-            second = third;
-            third = result;
-        }
+        if(map.containsKey(n)) return map.get(n);
 
-        return result;
+        map.put(n, tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3));
+        return map.get(n);
     }
 }
