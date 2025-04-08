525946d33d1c97a5eca14eadbad913daa38a94ed
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-04-08 12:12:33 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,11 @@
 class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        if(n == 0 || n == 1 || n == 2) return n;
+        if(map.containsKey(n)) return map.get(n);
 
-        int prev1 = 1;
-        int prev2 = 2;
-
-        for(int i = 3; i <=n; i++){
-            int current = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = current;
-        }
-
-        return prev2;
+        map.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+        return map.get(n);
     }
 }
