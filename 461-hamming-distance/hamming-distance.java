39ba432a94616ab1b7b5cb9437cdccb5d5ada56e
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-04-13 17:40:44 -0400

    Time: 0 ms (100.00%) | Memory: 40.6 MB (51.66%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,12 +1,11 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int count = 0;
+        int xor = x ^ y;
 
-        while(x > 0 || y > 0){
-            if((x & 1) != (y & 1))
-                count++;
-            x >>= 1;
-            y >>= 1;
+        while(xor > 0){
+            count += xor & 1;
+            xor >>= 1;
         }
 
         return count;
