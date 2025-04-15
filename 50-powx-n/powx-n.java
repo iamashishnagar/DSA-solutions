1cb4a55b795f93f5e094b4291b90085b00664912
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-04-15 16:19:04 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (46.22%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+
+        double result = helper(x, Math.abs(n));
+        return n >= 0 ? result : 1 / result;
+    }
+
+    private double helper(double x, int n){
+        if(n == 0) return 1;
+
+        double half = helper(x, n / 2);
+        double result = half * half;
+        return n % 2 == 0 ? result : x * result;
+    }
+}
