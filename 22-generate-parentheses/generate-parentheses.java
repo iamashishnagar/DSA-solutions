a4e11e0841d632dc4c5167d77d18fbcb349dc1fc
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-05-02 22:39:54 -0400

    Time: 2 ms (46.74%) | Memory: 43.4 MB (35.63%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,28 +1,18 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        if(n == 0) return null;
         List<String> result = new ArrayList<>();
-        Deque<Character> stack = new ArrayDeque<>();
-        backtrack(0, 0, n, result, new StringBuilder());
+        backtracking(result, "", 0, 0, n);
         return result;
     }
 
-    private void backtrack(int open, int close, int n, List<String> result, StringBuilder current){
-        if(open == close && open == n){
-            result.add(current.toString());
-            return;
-        }
-
-        if(open < n){
-            current.append('(');
-            backtrack(open + 1, close, n, result, current);
-            current.deleteCharAt(current.length() - 1);
-        }
-
-        if(close < open){
-            current.append(')');
-            backtrack(open, close + 1, n, result, current);
-            current.deleteCharAt(current.length() - 1);
-        }
+    private void backtracking(List<String> result, String current, int open, int close, int max){
+        if(current.length() == 2 * max)
+            result.add(current);
+        
+        if(open < max)
+            backtracking(result, current + "(", open + 1, close, max);
+        
+        if(close < open)
+            backtracking(result, current + ")", open, close + 1, max);
     }
 }
