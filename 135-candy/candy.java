aae144d3c6deb81f9958ab7ced5d93ab5ac5e634
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-05-03 14:45:58 -0400

    Time: 3 ms (84.71%) | Memory: 45.6 MB (80.74%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if(n == 0) return 0;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        //left to right pass
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        }
+
+        //right to left pass
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1])
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+
+        int numberOfCandies = 0;
+        for(int candy : candies)
+            numberOfCandies += candy;
+        
+        return numberOfCandies;
+    }
+}
