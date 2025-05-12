e786d66bf061718fd98a9c17ebf7018a03542ca2
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-05-12 14:51:09 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (24.36%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
+        int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
         int prev1 = 0;
         int prev2 = 0;
 
