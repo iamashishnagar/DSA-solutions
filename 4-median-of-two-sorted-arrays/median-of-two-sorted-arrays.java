09ab4ef5126db75cc1907ffb0407cfa6b9f2f5d0
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-05-12 11:45:25 -0400

    Time: 1 ms (100.00%) | Memory: 46.3 MB (25.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,8 +1,11 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
+        int m = nums1.length;
+        int n = nums2.length;
         if(m > n) return findMedianSortedArrays(nums2, nums1);
-        int leftHalf = (m + n + 1) / 2;
+
+        int totalLength = m + n;
+        int leftHalf = (totalLength + 1) / 2;
         int left = 0, right = m;
 
         while(left <= right){
@@ -10,19 +13,19 @@ class Solution {
             int mid2 = leftHalf - mid1;
 
             int leftMax1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[mid1 - 1];
-            int leftMax2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2 - 1];
             int rightMin1 = mid1 == m ? Integer.MAX_VALUE : nums1[mid1];
+            int leftMax2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2 - 1];
             int rightMin2 = mid2 == n ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(leftMax1 <= rightMin2 && leftMax2 <= rightMin1)
-                return (m + n) % 2 != 0
-                    ? Math.max(leftMax1, leftMax2)
-                    : (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2.0;
-            else if(leftMax1 > rightMin2)
-                right = mid1 - 1;
-            else left = mid1 + 1;
+            if(leftMax1 <= rightMin2 && leftMax2 <= rightMin1){
+                if(totalLength % 2 == 0)
+                    return (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2.0;
+                else return Math.max(leftMax1, leftMax2);
+            }
+            else if(leftMax1 > rightMin2) right--;
+            else left++;
         }
 
-        throw new IllegalArgumentException("Input arrays are not sorted.");
+        return 0.0;
     }
 }
