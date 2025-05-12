88628fe0474e736be145a1ca530d1d142d9a0be4
Ashish Nagar <70322738+iamashishnagar@users.noreply.github.com>
2025-05-12 12:37:27 -0400

    Time: 27 ms (85.65%) | Memory: 50.7 MB (99.79%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,15 +5,16 @@ class Solution {
 
         for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i + 1;
-            int right = nums.length - 1;
+            int left = i + 1, right = nums.length - 1;
 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
-                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1])
+                        left++;
                 }
                 else if(sum < 0) left++;
                 else right--;
